2a4f1fc9ac5a255721eb125cbe900bc27353e652
RashikShahjahan <rashikshahjahan@protonmail.com>
2017-07-18 12:01:27 -0400

    Huge commit

---
+++ app/src/main/java/com/example/abmshahjahan/bro/appOpener.java
@@ -0,0 +1,50 @@
+package com.example.abmshahjahan.bro;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
+/**
+ * Created by ABM Shahjahan on 2017-06-02.
+ */
+
+public class appOpener {
+
+    public String getPackNameByAppName(Context context, String name) {
+        PackageManager pm = context.getPackageManager();
+        List<ApplicationInfo> l = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        String packName = "";
+        for (ApplicationInfo ai : l) {
+            String n = (String) pm.getApplicationLabel(ai);
+            if (n.contains(name) || name.contains(n)) {
+                packName = ai.packageName;
+            }
+        }
+
+        return packName;
+    }
+
+    /**
+     * Open another app.
+     *
+     * @param context     current Context, like Activity, App, or Service
+     * @param packageName the full package name of the app to open
+     * @return true if likely successful, false if unsuccessful
+     */
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+
+        //throw new PackageManager.NameNotFoundException();
+        Intent i = manager.getLaunchIntentForPackage(packageName);
+        if (i == null) {
+            return false;
+
+        }
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        context.startActivity(i);
+        return true;
+    }
+}
